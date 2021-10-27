3163c710482b1c3be5b88808331cb9ed8c3b10d6
chandan-l <80918079+chandan-l@users.noreply.github.com>
2021-10-27 19:56:42 +0530

    Create if_else.java

---
+++ if_else.java
@@ -0,0 +1,24 @@
+public class Conditional_If_Else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int b = sc.nextInt();
+
+        if(a > b){
+            System.out.println("A is Greater");
+        }
+        else if(b > a){
+            System.out.println("B is Greater");
+        }
+        else{
+            System.out.println("Both are equal");
+        }
+
+        //Ternary Operator
+        System.out.println(a > b ? a : b + "is greater");
+        sc.close();
+    }
+}
